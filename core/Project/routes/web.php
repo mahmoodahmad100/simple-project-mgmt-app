29d59cadd7afcd78092a274a11e2f92e983cdcdf
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-13 19:49:23 +0200

    not needed module

--- core/Project/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::namespace('Core\Project\Controllers\Web')->prefix('admin')->name('admin.')->group(function () {
-    #*** Ex: START: Project ***#
-    // Route::resource('projects', 'ProjectController')->except([
-    //    'store', 'update', 'destroy'
-    // ]);
-    #*** END: Project ***#
-});
